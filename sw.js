c546b0e2c0f26b9160a3211f63c8385595a1cb10
markolati85 <markolatinovic1234@gmail.com>
2025-09-27 11:39:16 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "dotrush-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./privacy.html"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
